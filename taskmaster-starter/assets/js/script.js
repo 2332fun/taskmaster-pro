fd4c4d37822867deec4b7213ebfb0768ef29b8fb
DCT <mirachan2332@gmail.com>
2021-12-20 11:34:50 -0600

    added drag and drop

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -13,7 +13,6 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
-
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -102,7 +101,6 @@ $(".list-group").on("blur", "textarea", function() {
   //get the textarea's current value/text
   var text = $(this)
   .val()
-  .trim();
 
   //get the parent ul's id attribute
   var status = $(this)
@@ -185,6 +183,59 @@ $("#remove-tasks").on("click", function() {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+//turns every element with class list-group into sortable list. connectWith links sortable lists with other lists with same class.
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+  // array to store the task data in
+  var tempArr = [];
+
+  // loop over current set of children in sortable list
+  $(this).children().each(function() {
+    var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+    var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+    // add task data to the temp array as an object
+    tempArr.push({
+      text: text,
+      date: date
+    });
+  });
+  // trim down list's ID to match object property
+var arrName = $(this)
+.attr("id")
+.replace("list-", "");
+
+// update array on tasks object and save
+tasks[arrName] = tempArr;
+saveTasks();
+
+  console.log(tempArr);
+    }
 });
 
 // load tasks for the first time
